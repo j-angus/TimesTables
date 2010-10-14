69aa870862e28956bfca7f39ff1d26b0451a6ebf
Jason Angus <duff.angus@gmail.com>
2010-10-15 03:04:08 +0800

    Used switch code block to replace if...else logic. Tidy up code.

--- Menu.java
+++ Menu.java
@@ -1,23 +1,22 @@
 /**
- *  Some code to provide menu displaying for
- *    TimesTables program... :)
+ *  Some code to provide menu displaying for TimesTables program... :)
+ * 
  * Author:     Jason Angus
  * File:       Menu.java
- * Last Edit:  10/10/2010
+ * Last Edit:  15/10/2010
  */
 
 import java.util.*;
 
-
 public class Menu {
 
    private int menuSelection = 0;
+   private boolean validChoice = false;
    public static final int MENU_MAX = 3;
    public static final int MENU_MIN = 1;
-   private boolean validChoice = false;
 
    public void displayMenu() {
-      System.out.println("Times Tables Tester!");
+      System.out.println("Times Tables Tester.\n");
       System.out.println("1. Single Times Table");
       System.out.println("2. All Times Tables");
       System.out.println("3. Exit");
@@ -40,55 +39,60 @@ public class Menu {
             break;
          default:
             this.validChoice = false;
-            System.out.println("\nERROR: Not a valid choice!");
+            System.out.println("\nERROR: Not a valid choice!\n");
       }
       return;
    } // close  getMenuSelection method
 
    public void goGoGO() {
-      if ( this.validChoice ) {
-         if (this.menuSelection == 1) {
-            goChoice1();
-         } // close option 1
-         else if (this.menuSelection == 2) {
-            goChoice2();
-         } // close option 2
-         else if (this.menuSelection == 3) {
-            goChoice3();
+      if ( this.validChoice == true) {
+         switch (this.menuSelection) {
+            case 1:
+               goChoice1();
+               break;
+            case 2:
+               goChoice2();
+               break;
+            case 3:
+               goChoice3();
+               break;
          }
-      } // close outer if
-   } // close goGoGO method
+      }
+      return;
+   } // close goGoGO() method
 
    public void goChoice1() {
-      // ask user for the table they want to test then do it!
+      // Ask user for the table they want to test then do it!
       while (true) {
          System.out.println("Single Times Tables! - Menu Item #1\n");
          System.out.print("Enter table to attempt (1 - 12), or zero to quit: ");
          Scanner in = new Scanner(System.in);
          int choice = 0;
-         if (in.hasNextInt()) // Confirm user has entered an integer.
+         
+         if (in.hasNextInt()) // Confirm user has entered an integer
             choice = in.nextInt();
          else
             System.out.println("\nERROR: Invalid choice!\n");
-         if (choice == 0)
+            
+         if (choice == 0) // User wants to quit
             break;
-         else if (choice >= TimesTable.TABLE_MIN &&
+            
+         if (choice >= TimesTable.TABLE_MIN && 
                   choice <= TimesTable.TABLE_MAX) {
             TimesTable myTable = new TimesTable(choice);
-            System.out.println("You chose " + choice + " times table");
+            System.out.println("You chose " + choice + " times table.\n");
             int tries = 0;
-            for (int i=TimesTable.TABLE_MIN; i<=TimesTable.TABLE_MAX; i++) {
-               int answer = -1;
+            int answer = 0;
+
+            for (int i=TimesTable.TABLE_MIN; i<=TimesTable.TABLE_MAX; ++i) {
                do {
                   myTable.displaySum(i);
                   
-                  // Check for integer input
-                  if (in.hasNextInt()) {
+                  if (in.hasNextInt()) { // Check for integer input
                      answer = in.nextInt();
                      in.nextLine(); // Throw away the buffer
                      ++tries;
-                  }
-                  else {
+                  } else {
                      System.out.println("\nERROR: Not an integer!!\n");
                      in.nextLine(); // Throw away the buffer
                   }
@@ -96,18 +100,20 @@ public class Menu {
             } // end for
             System.out.printf("Great! You took %d goes!\n", tries);
          } else {
-            System.out.println("Thats gotta be a table between 1 and 12 you Ninny!");
+            System.out.println("\nERROR: Table must be between " +
+                                 TimesTable.TABLE_MIN + " and " +
+                                 TimesTable.TABLE_MAX + ".\n");
          } // end else
       } // end while
-   } // close goChoice1 method
+   } // close goChoice1() method
 
    public void goChoice2() {
       // throw random problems at the user until they've had enough
-      System.out.println("You chose All Times Tables!! - menu #2");
+      System.out.println("You chose All Times Tables!! - Menu Item #2");
    } // close goChoice2 method
 
    public void goChoice3() {
-      System.out.println("You chose Exit!!! - menu #3");
+      System.out.println("You chose Exit!!! - Menu Item #3");
       System.out.println("Goodbye then!");
       System.exit(0);
    } // close goChoice3 method
